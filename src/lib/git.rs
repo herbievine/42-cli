5e54d3c962fc05406044860188f370e684fcf27c
Herbie Vine <vineherbie@gmail.com>
2023-02-14 11:32:41 +0100

    start update feature

--- src/lib/git.rs
+++ src/lib/git.rs
@@ -1,6 +1,10 @@
 use super::process::{exec_commands, ExecuteCommandError};
 use std::path::Path;
 
+pub fn init<P: AsRef<Path>>(target_dir: P) -> Result<(), ExecuteCommandError> {
+    exec_commands(vec!["git init"], target_dir)
+}
+
 pub fn add<P: AsRef<Path>>(target_dir: P) -> Result<(), ExecuteCommandError> {
     exec_commands(vec!["git add ."], target_dir)
 }
@@ -13,8 +17,9 @@ pub fn push<P: AsRef<Path>>(target_dir: P) -> Result<(), ExecuteCommandError> {
     exec_commands(vec!["git push -u origin main"], target_dir)
 }
 
-pub fn init<P: AsRef<Path>>(target_dir: P) -> Result<(), ExecuteCommandError> {
-    exec_commands(vec!["git init"], target_dir)
+pub fn clone<P: AsRef<Path>>(target_dir: P, remote: &str) -> Result<(), ExecuteCommandError> {
+    let command = format!("git clone {}", remote);
+    exec_commands(vec![&command], target_dir)
 }
 
 pub fn add_remote<P: AsRef<Path>>(target_dir: P, remote: &str) -> Result<(), ExecuteCommandError> {
