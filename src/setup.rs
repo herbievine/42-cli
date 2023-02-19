813c6e57c130d688bf1d56dbd79c2bb350190eb6
Herbie Vine <vineherbie@gmail.com>
2023-02-19 11:40:32 +0100

    replace absolute path to relative

--- src/setup.rs
+++ src/setup.rs
@@ -2,14 +2,21 @@ use figlet_rs::FIGfont;
 
 pub fn setup() {
     print!("\x1B[2J\x1B[1;1H");
-    print!("\n\n");
+    print!("\n");
 
-    let small_font = FIGfont::from_file("src/resources/alligator.flf").unwrap();
-    let figure = small_font.convert("42  CLI");
+    let font_bytes = include_bytes!("resources/alligator.flf");
 
-    if figure.is_some() {
-        println!("{}", figure.unwrap());
+    match FIGfont::from_content(&String::from_utf8_lossy(font_bytes)) {
+        Ok(font) => {
+            let figure = font.convert("42  CLI");
+            if figure.is_some() {
+                println!("{}", figure.unwrap());
+            }
+        }
+        _ => {
+            println!("42 CLI");
+        }
     }
 
-    print!("\n\n");
+    print!("\n");
 }
