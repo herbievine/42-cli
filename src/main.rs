892fdd51a2950402da25df1027515b038dcc2d4b
Herbie Vine <vineherbie@gmail.com>
2023-05-08 19:23:50 +0200

    da

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,24 @@ fn main() {
     let cli = CliArgs::parse();
     let config = lib::config::get_config();
 
+    println!("{}", "42 CLI".bright_cyan().bold());
+
     let error = match cli.subcommand {
         Commands::Test(args) => test::exec(&args, &config),
     };
 
-    if error.exit_code != 0 {
-        println!("{} {}", "Error running: ".red().bold(), error.command.red());
-        println!("{} {}", "Trace: ".red().bold(), error.trace.red());
+    if !error.exit_code != 0 {
+        println!(
+            "{} `{}`",
+            "Error in".bright_red().bold(),
+            config.name.bright_red().bold()
+        );
+        println!(
+            "{} `{}`",
+            "Error running:".red().bold(),
+            error.command.red()
+        );
+        println!("{} {}", "Trace:".red().bold(), error.trace.red());
         std::process::exit(error.exit_code);
     } else {
         println!("{}", "Done!".bright_green().bold());
